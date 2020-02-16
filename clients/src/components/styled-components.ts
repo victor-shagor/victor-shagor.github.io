6ea68d8f917377f8d41862b2112ea6d7f5bd261c
abiola ojo <ojoabiolavictor@gmail.com>
2020-02-16 09:35:55 +0100

    landing page

---
+++ clients/src/components/styled-components.ts
@@ -0,0 +1,242 @@
+import styled from 'styled-components';
+import bookings from '../connect-assests/travel_booking.png';
+
+export const NavDiv = styled.div`
+background: url(${bookings});
+background-repeat: no-repeat;
+background-position: right -50px top -34px;
+background-size: 1000px;
+height: 60vh;
+h3{
+    font-size: 30px;
+    margin-top: 20px;
+    margin-left: 15vh;
+}
+.nav{
+    display: flex;
+    margin-top: -70px;
+}
+ul{
+float: right;
+display:flex;
+list-style: none;
+margin-top: 0px;
+margin-left: 880px;
+}
+
+li a{
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size:16px;
+    font-family: 'Open Sans', sans-serif;
+    letter-spacing: 0;
+    color: #C4C9D9;
+    cursor: pointer;
+}
+li a:hover{
+    background-color: #00BFA6;
+    border-radius: 10%;
+    border: none;
+    width: 150px;
+    height: 40px;
+    color: white;
+}
+.middle{
+    margin-top: 150px;
+    text-align: left;
+    margin-left: 20vh;
+    color: #31384E; 
+}
+
+.middle .h1{
+    font-size:50px;
+    font-family: 'Open Sans', sans-serif;
+    color: #31384E;
+    font-weight: bold;
+}
+.middle .h2{
+font-size: 31px;
+font-family: 'Open Sans', sans-serif;
+color: #31384E;
+font-weight: lighter;
+}
+.middle .p{
+text-align: left;
+font-family: 'Open Sans', sans-serif;
+font-size: 18px;
+color: #697598;
+opacity: 0.8;
+font-weight: normal;
+}
+.middle input{
+    background-color: #00BFA6;
+    border-radius: 10%;
+    border: none;
+    width: 150px;
+    height: 40px;
+    color: white;
+    cursor: pointer;
+    }
+    .middle input:hover{
+        background-color: white;
+        color: #00BFA6;
+        border: 1px solid #00BFA6;
+        }
+`
+
+export const SectionDiv = styled.div`
+.section2{
+    display: flex;
+    margin-left: 20vh;
+    margin-top: 86px;
+    margin-bottom: 30px;
+}
+
+.section2 .back1{
+height: 250px;
+width: 348px;
+opacity: 1;
+background: transparent linear-gradient(134deg, #00BFA6 0%, #83E0BD 100%) 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 50px #31384E1A;
+margin-right: 37px;
+padding-left: 15px;
+padding-right:15px;
+color: white;
+text-align: center;
+transition: transform .5s;
+}
+.back1:hover{
+    transform: scale(1.1); 
+}
+.section2 .back2{
+height: 250px;
+width: 348px; 
+opacity: 1; 
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 50px #31384E14; 
+margin-right: 37px;
+padding-left:15px;
+padding-right:15px;
+text-align: center;
+transition: transform .5s;
+}
+.back2:hover{
+    transform: scale(1.1); 
+}
+.section2 .back3{
+height: 250px;
+width: 348px; 
+opacity: 1; 
+background: #FFFFFF 0% 0% no-repeat padding-box;
+box-shadow: 0px 10px 50px #31384E14; 
+padding-left:15px; 
+padding-right:15px; 
+text-align: center;
+transition: transform .5s;
+}
+.back3:hover{
+    transform: scale(1.1); 
+}
+.section2 p{
+    padding-bottom:10px;
+    font-size: 13px;
+    font-family: 'Open Sans', sans-serif;
+
+}
+.header{
+    font-weight: 800;
+}
+img{
+    margin-top: 20px;
+}
+
+`
+export const Services = styled.div`
+text-align: center;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ margin-top: 15vh;
+ margin-bottom: -60px;
+ justify-content: center;
+.services .h1{
+    color:#00BFA6;
+    font-family: 'Open Sans', sans-serif;
+    }
+    .services .h2{
+     font-weight: bold;
+     font-family: 'Open Sans', sans-serif;
+    
+    }
+`
+export const Section1Div = styled.div`
+display: flex;
+.img1{
+    margin-left: 10vh;
+    margin-right: 10px;
+    margin-top: -15px;
+}
+.img2{
+    text-align: center;
+    margin-right: 97px;
+    margin-top: 50px;
+}
+.top{
+    font-weight: 500;
+    font-size: 40px;
+    width: 400px;
+    height: 97px;
+}
+.btw{
+    padding-top: 10px;
+}
+input{
+    background-color: #00BFA6;
+    border-radius: 10%;
+    border: none;
+    width: 150px;
+    height: 40px;
+    color: white;
+    cursor: pointer;
+    }
+   input:hover{
+        background-color: white;
+        color: #00BFA6;
+        border: 1px solid #00BFA6;
+        }
+`
+
+export const FooterDiv = styled.div`
+    display: flex;
+    height: 30vh;
+    background-color: #00BFA6;
+    color: white;
+
+.secti1{
+    margin-top: 5vh;
+    margin-left: 280px;
+    margin-right: 150px;
+}
+.secti2{
+    margin-top: 2vh;
+    margin-right: 130px;
+}
+.secti3{
+    margin-top: 2vh;
+    margin-right: 130px;
+}
+.secti4{
+    margin-top: 2vh;
+    margin-right: 50px;
+}
+img{
+    height: 30px;
+    margin:2px;
+    transition: transform .2s;
+}
+img:hover{
+    transform: scale(1.1); 
+}
+`
